195bb33497f91e63e6c3e5a18f51ca379708741e
runar.persen <runar.persen@.dnb.no>
2023-06-06 12:36:02 +0200

    .

--- jaffle_shop/models/test_func_eng/lineitem_fact.sql
+++ jaffle_shop/models/test_func_eng/lineitem_fact.sql
@@ -2,7 +2,7 @@ with li as(
     select * from {{ ref('lineitem') }}
 ),
 final as(
-    select to_char(l_receiptdate,'yyyymmdd') as periodid, 
+    select to_number(to_char(l_receiptdate,'yyyymmdd')) as periodid, 
         l_linenumber as linenumber, 
         l_quantity as quantity, 
         l_extendedprice as extendedprice 
